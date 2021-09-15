9eca2d9181e3abbdebfa351d1318a09db74203cc
Roman <swany500@gmail.com>
2021-09-15 18:42:28 +0300

    CLoud Server Config

---
+++ api-gateway/src/main/java/epam/com/api/gateway/TempFixConfig.java
@@ -0,0 +1,16 @@
+package epam.com.api.gateway;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.netty.resolver.DefaultAddressResolverGroup;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class TempFixConfig {
+
+    @Bean
+    public HttpClient webClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+}
